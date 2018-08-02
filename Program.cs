ac64b088b484654eff3330d4b105b4a0ad04c6be
Sidney Andrews <sidney@seesharprun.net>
2018-08-02 18:46:54 -0400

    Check-it-in

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Humanizer;
+
+namespace RVNUG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string timeTill2019 = new DateTime(2019, 01, 01).Humanize();
+            Console.WriteLine(timeTill2019);
+        }
+    }
+}
